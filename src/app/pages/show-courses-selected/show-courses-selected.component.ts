c66de252e489a2dc4c6f29036be4c90911d871a4
bxnjadev <benjamin.hyx@gmail.com>
2026-08-11 14:20:18 -0400

    feat: addd link in courses

--- src/app/pages/show-courses-selected/show-courses-selected.component.ts
+++ src/app/pages/show-courses-selected/show-courses-selected.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Schedule } from '../../services/schedule';
 import { NgFor } from '@angular/common';
 import { CourseSchedule } from '../../model/course';
+import { ViewCourseDelegator } from '../../delegator/view.course.delegator';
 
 @Component({
   selector: 'app-show-courses-selected',
@@ -12,10 +13,14 @@ import { CourseSchedule } from '../../model/course';
 export class ShowCoursesSelectedComponent {
 
   private readonly schedule : Schedule = inject(Schedule);
-
+  private viewCourseDelegator = inject(ViewCourseDelegator);
 
   public getSchedule() : Schedule {
     return this.schedule;
+  }
+
+  public searchCourse(id : number) : void {
+    this.viewCourseDelegator.update(id);
   }
 
   public deleteCourse(id : number) : void {
