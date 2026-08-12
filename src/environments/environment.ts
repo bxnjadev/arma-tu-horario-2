135a915187a1ae872219b87fabf3248137c48c10
bxnjadev <benjamin.hyx@gmail.com>
2026-08-12 17:01:47 -0400

    chore: updated url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: "https://alicahue-webapi-git-main-bxnjadevs-projects.vercel.app/api/"
+    apiUrl: "https://alicahue-webapi.onrender.com/api/"
 };
